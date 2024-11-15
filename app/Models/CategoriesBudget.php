c1f8af088ecbf5b200fc6175f841f86990655e57
Gian Mateus <gian.m.silver@gmail.com>
2024-11-15 19:13:51 -0300

    Refatorar estrutura do projeto e internacionalizar nomes
    - Remover modelos não utilizados
    - Renomear tabelas e colunas para inglês
    - Atualizar rotas e controllers
    - Simplificar estrutura de navegação no sidebar
    - Remover configurações redundantes
    - Adicionar rota e controller para extratos

--- app/Models/CategoriesBudget.php
+++ app/Models/CategoriesBudget.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Banco extends Model
+class CategoriesBudget extends Model
 {
     use HasFactory;
     // protected $fillable = [
-    //     'nome',
-    //     'agencia',
-    //     'conta'
+    //     'categoria_id',
+    //     'valor_orcado',
+    //     'mes_referencia'
     // ];
 }
