207df022530939fefef38358f8147d19ca301660
Gian Mateus <gian.m.silver@gmail.com>
2024-11-16 10:30:08 -0300

    - Cria StatementsController com método index - Remove componente sidebar não utilizado - Atualiza nome do aplicativo para 'Smart Finance' em config/app.php - Implementa layout base com sidebar, menu e estrutura responsiva - Remove view de boas-vindas não utilizada

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,14 +1,63 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body class="min-h-screen font-sans antialiased bg-base-200/50 dark:bg-base-200">
-    
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
+		<meta name="csrf-token" content="{{ csrf_token() }}">
+		<title>{{ isset($title) ? $title . ' - ' . config('app.name') : config('app.name') }}</title>
+
+		@vite(['resources/css/app.css', 'resources/js/app.js'])
+	</head>
+
+	<body class="min-h-screen bg-base-200/50 font-sans antialiased dark:bg-base-200">
+		{{-- NAVBAR mobile only --}}
+		<x-nav sticky class="lg:hidden">
+			<x-slot:brand>
+				<div class="ml-5 pt-5">App</div>
+			</x-slot:brand>
+			<x-slot:actions>
+				<label for="main-drawer" class="mr-3 lg:hidden">
+					<x-icon name="o-bars-3" class="cursor-pointer" />
+				</label>
+			</x-slot:actions>
+		</x-nav>
+
+		{{-- MAIN --}}
+		<x-main full-width>
+			{{-- SIDEBAR --}}
+			<x-slot:sidebar drawer="main-drawer" collapsible collapse-text="Ocultar" class="bg-base-100 lg:bg-inherit">
+
+				{{-- BRAND --}}
+				<div class="ml-4 flex items-center pt-4">
+					<x-icon name="s-currency-dollar" class="w-10" />
+					<div class="px-4 font-bold">SMART FINANCE</div>
+				</div>
+
+				{{-- MENU --}}
+				<x-menu activate-by-route>
+					<x-menu-item title="Dahsboard" icon="o-chart-pie" link="/" />
+					<x-menu-item title="Extrato" icon="c-arrows-up-down" link="{{ route('extrato.index') }}" />
+					<x-menu-item title="Recorrentes" icon="o-clock" link="/" />
+					<x-menu-item title="Importar arquivo" icon="o-document-arrow-up" link="/" />
+					<x-menu-item title="Perfil" icon="o-user-circle" />
+                    <x-menu-sub title="Configurações" icon="o-cog-6-tooth">
+                        <x-menu-item title="Categorias/Subcategorias" icon="c-squares-plus" link="/" />
+                        <x-menu-item title="Orçamentos" icon="o-document-currency-dollar" link="/" />
+                        <x-menu-item title="Periodicidades" icon="m-calendar" link="/" />
+                        <x-menu-item title="Tipos de Transção" icon="c-list-bullet" link="/" />
+                    </x-menu-sub>
+				</x-menu>
+			</x-slot:sidebar>
+
+			{{-- The `$slot` goes here --}}
+			<x-slot:content>
+                {{ isset($slot) ? $slot : "Nada aqui" }}
+			</x-slot:content>
+		</x-main>
+
+		{{-- Toast --}}
+		<x-toast />
+	</body>
+
 </html>
---
+++ resources/views/statements/index.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.app>
+    <h1>Teste</h1>
+</x-layouts.app>
