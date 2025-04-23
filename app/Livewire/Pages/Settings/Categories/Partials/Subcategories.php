f1ce9de3276ae7ef5b5010bb8f251b47d6c4f780
Gian Mateus <gian.m.silver@gmail.com>
2025-04-22 22:46:46 -0300

    toast confirmação de delete subcategoria

--- app/Livewire/Pages/Settings/Categories/Partials/Subcategories.php
+++ app/Livewire/Pages/Settings/Categories/Partials/Subcategories.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Pages\Settings\Categories\Partials;
 
+use Mary\Traits\Toast;
 use Livewire\Component;
 use App\Models\Subcategory;
 use Illuminate\Support\Facades\Auth;
@@ -9,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 class Subcategories extends Component
 {
     public Subcategory $subcategory;
+    use Toast;
 
     public function delete()
     {   
@@ -17,6 +19,7 @@ class Subcategories extends Component
 
         // Dispatch event to refresh categories
         $this->dispatch('refreshCategories');
+        $this->success('Subcategoria excluída com sucesso!');
     }
 
     public function render()
