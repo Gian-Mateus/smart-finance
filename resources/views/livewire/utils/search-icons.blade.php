2b77d70ffe7bd76d6e350b919c66e303db00c61e
Gian Mateus <gian.m.silver@gmail.com>
2025-04-25 17:36:51 +0000

    feat: dropdown in search-icons

--- resources/views/livewire/utils/search-icons.blade.php
+++ resources/views/livewire/utils/search-icons.blade.php
@@ -1,3 +1,16 @@
 <div>
-    {{ dd($icons) }}
+    <x-dropdown>
+        <x-slot:trigger>
+            <x-button icon="o-photo" class="btn" />
+        </x-slot:trigger>
+     
+        <x-menu-item>
+            <x-input placeholder="Pesquisar" icon="o-magnifying-glass" />
+        </x-menu-item>
+        @foreach ($icons as $icon)
+        <x-menu-item class="block">
+            <x-icon name="{{ $icon }}" />
+        </x-menu-item>
+        @endforeach
+    </x-dropdown>
 </div>
