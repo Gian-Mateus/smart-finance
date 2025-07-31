4a471629f3348547963d3b2e73ceb00116be018f
Gian Mateus <gian.m.silver@gmail.com>
2025-07-31 20:29:06 -0300

    desgraça de dropdown do caralho

--- resources/views/livewire/pages/settings/categories/categories-index.blade.php
+++ resources/views/livewire/pages/settings/categories/categories-index.blade.php
@@ -25,82 +25,87 @@
 		labelButton="Nova Categoria"
 		placeholderInput="Nome da Categoria"
 	/>
-
-	@foreach ($this->categories as $category)
-		<x-collapse
-			class="group m-0.5 bg-base-100"
-			separator
-			id="{{ uniqid() }}"
-			x-data="{
-				name: '{{ strtolower($category->name) }}'
-			}"
-			x-show="$data.name.includes($data.search.toLowerCase())"
-		>
-			<x-slot:heading>
-				@if ($category->icon)
-					<x-icon name="{{ $category->icon }}"/>
-				@endif
-				<h4>
-					{{ $category->name }}
-				</h4>
-				
-				<x-dropdown class="z-10">
-					<x-slot:trigger>
-						<x-button icon="m-ellipsis-vertical" class="btn-ghost hover:bg-red-500"/>
-					</x-slot:trigger>
-				
-					<x-button
-						class="btn-sm"
-						icon="o-trash"
-						responsive
-					/>
-					<x-button 
-						class="btn-sm"
-						icon="c-pencil"
-						responsive
-						x-show="!$data.selectCategory"
-						wire:click="openModalEdit({{ $category }})"
-					/>
-				</x-dropdown>
-			</x-slot:heading>
-			<x-slot:content>
-				<ul>
-					<li>
-						<livewire:pages.settings.categories.partials.add
-							type="subcategory"
-							labelButton="Nova Subcategoria"
-							placeholderInput="Nome da Subcategoria"
-							:category_id="$category->id"
-							:key="uniqid()"
-						/>
-					</li>
-					{{-- Subcategories --}}
-					@foreach ($category->subcategories as $subcategory)
-					<li class="group/subcat relative flex items-center justify-between rounded p-2 hover:bg-base-300">
-						<div>
-							{{ $subcategory->name }}
+	<div class="flex flex-col gap-1">
+		@foreach ($this->categories as $category)
+			<x-collapse
+				class="w-full overflow-visible relative z-10"
+				separator
+				id="{{ uniqid() }}"
+				x-data="{
+					name: '{{ strtolower($category->name) }}'
+				}"
+				x-show="$data.name.includes($data.search.toLowerCase())"
+			>
+				<x-slot:heading>
+					<div class="flex items-center justify-between">
+						<div class="flex items-center gap-2">
+							@if ($category->icon)
+								<x-icon name="{{ $category->icon }}"/>
+							@endif
+							<h4>
+								{{ $category->name }}
+							</h4>
 						</div>
-						<div class="flex items-center justify-between pr-6 gap-2 opacity-0 transition-opacity duration-300 group-hover/subcat:opacity-100">
-							{{-- Button Edit SubCategory --}}
-							<x-button
-								class="btn-sm"
-								icon="c-pencil"
-								responsive
-								wire:click="openModalEdit({{ $subcategory }})"
+						<x-dropdown no-x-anchor right>
+							<x-slot:trigger>
+								<x-button icon="m-ellipsis-vertical" class="z-20 absolute -right-6 top-1/2 -translate-y-1/2 btn-ghost"/>
+							</x-slot:trigger>
+							<div class="flex flex-col gap-1 z-80">
+								<x-menu-item
+									icon="o-trash"
+									responsive
+									title="Excluir"
+								/>
+								<x-menu-item
+									icon="c-pencil"
+									label="Editar"
+									responsive
+									x-show="!$data.selectCategory"
+									wire:click="openModalEdit({{ $category }})"
+								/>
+							</div>
+						</x-dropdown>
+					</div>
+				</x-slot:heading>
+				<x-slot:content>
+					<ul>
+						<li>
+							<livewire:pages.settings.categories.partials.add
+								type="subcategory"
+								labelButton="Nova Subcategoria"
+								placeholderInput="Nome da Subcategoria"
+								:category_id="$category->id"
+								:key="uniqid()"
 							/>
-							<x-button
-								class="btn-sm"
-								icon="o-trash"
-								responsive
-								wire:click="deleteSubcategory({{ $subcategory->id }})"
-							/>
-						</div>
-					</li>
-					@endforeach
-				</ul>
-			</x-slot:content>
-		</x-collapse>
-	@endforeach
+						</li>
+						{{-- Subcategories --}}
+						@foreach ($category->subcategories as $subcategory)
+						<li class="group/subcat relative flex items-center justify-between rounded p-2 hover:bg-base-300">
+							<div>
+								{{ $subcategory->name }}
+							</div>
+							<div class="flex items-center justify-between pr-6 gap-2 opacity-0 transition-opacity duration-300 group-hover/subcat:opacity-100">
+								{{-- Button Edit SubCategory --}}
+								<x-button
+									class="btn-sm"
+									icon="c-pencil"
+									responsive
+									wire:click="openModalEdit({{ $subcategory }})"
+								/>
+								<x-button
+									class="btn-sm"
+									icon="o-trash"
+									responsive
+									wire:click="deleteSubcategory({{ $subcategory->id }})"
+								/>
+							</div>
+						</li>
+						@endforeach
+					</ul>
+				</x-slot:content>
+			</x-collapse>
+		@endforeach
+	</div>
 
 	{{-- Modal Confirm Delete --}}
 	<x-modal 
