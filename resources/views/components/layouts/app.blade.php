4695f27c9e857ee0bafbb3c094c385f382d9fd5f
gian.m.silver <gian.m.silver@gmail.com>
2025-08-06 19:43:45 +0000

    refact: alterando o nome da rota Periodicidades para Recorrências

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -52,8 +52,8 @@
 					<x-menu-sub title="Configurações" icon="o-cog-6-tooth">
 						<x-menu-item title="Bancos/Contas" icon="c-building-library" link="{{ route('banks') }}" />
 						<x-menu-item title="Categorias/Subcategorias" icon="c-squares-plus" link="{{ route('categories') }}" />
-						<x-menu-item title="Orçamentos/Metas" icon="o-document-currency-dollar" link="{{ route('budgets') }}" />
-						<x-menu-item title="Periodicidades" icon="m-calendar" link="/" />
+						<x-menu-item title="Orçamentos" icon="o-document-currency-dollar" link="{{ route('budgets') }}" />
+						<x-menu-item title="Recorrências" icon="m-calendar" link="/" />
 					</x-menu-sub>
 				</x-menu>
 			</x-slot:sidebar>
