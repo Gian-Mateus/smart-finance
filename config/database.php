61a5c0186e4c26f92ba667151443dbab08a4cb38
gian.m.silver <gian.m.silver@gmail.com>
2025-09-16 16:07:17 +0000

    feat: adiciona redis como banco de dados para filas

--- config/database.php
+++ config/database.php
@@ -112,6 +112,15 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => env('REDIS_QUEUE_CONNECTION', 'default'),
+            'queue' => env('REDIS_QUEUE', 'default'),
+            'retry_after' => env('REDIS_QUEUE_RETRY_AFTER', 90),
+            'block_for' => 5,
+            'after_commit' => false,
+        ],
+
     ],
 
     /*
