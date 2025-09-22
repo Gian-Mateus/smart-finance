4d2d905a279b3bce11c7fa6f040a3a64592d0fdc
gian.m.silver <gian.m.silver@gmail.com>
2025-09-22 23:18:25 +0000

    feat: adiciona lógica inicial da página de extratos bancários.

--- app/Livewire/Pages/Statements/Index.php
+++ app/Livewire/Pages/Statements/Index.php
@@ -2,10 +2,128 @@
 
 namespace App\Livewire\Pages\Statements;
 
+use Carbon\Carbon;
 use Livewire\Component;
+use App\Models\BanksAccount;
+use App\Models\Transaction;
+use App\MoneyBRL;
+use Livewire\Attributes\Computed;
+use Illuminate\Support\Facades\Auth;
 
 class Index extends Component
 {
+
+    use MoneyBRL;
+
+    public $account;
+    public $headers = [
+        ['key' => 'date_format', 'label' => 'Data'],
+        ['key' => 'description', 'label' => 'Descrição'],
+        ['key' => 'type_format', 'label' => 'Tipo'],
+        ['key' => 'value_format', 'label' => 'Valor'],
+    ];
+
+    public $month;
+    public $year;
+    public $range = 15;
+    public $dateRange;
+    public $configDatePicker = ['mode' => 'range', 'altFormat' => 'd/m/Y'];
+    public $initialDate;
+    public $endDate;
+
+    #[Computed]
+    public function accounts()
+    {
+        $accounts = BanksAccount::where('user_id', Auth::id())->get();
+
+        if(count($accounts) == 1){
+            $this->account = $accounts[0];
+        }
+        return $accounts;
+    }
+
+    #[Computed]
+    public function years()
+    {
+        $currentYear = Carbon::now()->year;
+        $years = [];
+        for ($i=0; $i <= 10; $i++) { 
+            $years[$i] = ['id' => $currentYear - $i, 'name' => $currentYear - $i];
+        }
+        return $years;
+    }
+
+    #[Computed]
+    public function months()
+    {
+        $months = [];
+        $labels = [
+            'Janeiro',
+            'Fevereiro',
+            'Março',
+            'Abril',
+            'Maio',
+            'Junho',
+            'Julho',
+            'Agosto',
+            'Setembro',
+            'Outubro',
+            'Novembro',
+            'Dezembro'
+        ];
+        for ($i=0; $i <= 11; $i++) { 
+            $months[$i] = ['id' => $i, 'name' => $labels[$i]];
+        }
+        return $months;
+    }
+
+    #[Computed]
+    public function transactions()
+    {
+        if($this->range != null){
+            $this->initialDate = Carbon::now();
+            $this->endDate = $this->initialDate->subDays($this->range);
+        }
+        
+        return Transaction::where('user_id', Auth::id())
+            ->where('bank_account_id', $this->account->id)
+            ->whereBetween('date', [$this->initialDate, $this->endDate])
+            ->get()
+            ->map(function ($i) {
+                $i->date_format = $i->date->format('d/m/Y');
+                $i->type_format = $i->type == 1 ? 'C' : 'D';
+                $i->value_format = $this->showBRL($i->value);
+                return $i;
+            });
+    }
+
+    public function filter()
+    {
+        //dd($this->dateRange);
+
+        if($this->dateRange){
+            $dateRange = explode('até', $this->dateRange);
+            $this->initialDate = $dateRange[0];
+            $this->endDate = $dateRange[1];
+            $this->transactions();
+            $this->reset();
+            return;
+        }
+
+        if($this->year != null && $this->month != null){
+            $this->initialDate = new Carbon($this->year, $this->month + 1, 1);
+            $this->endDate = new Carbon($this->year, $this->month + 1, 1);
+        }
+
+
+    }
+
+    public function mount()
+    {
+        $this->accounts();
+        $this->transactions();
+    }
+
     public function render()
     {
         return view('livewire.pages.statements.index');
--- resources/views/livewire/pages/statements/index.blade.php
+++ resources/views/livewire/pages/statements/index.blade.php
@@ -1,3 +1,41 @@
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    <x-header title="Extratos" subtitle="Aqui você pode ver todos os extratos das contas cadatradas" separator />
+
+    <div>
+        <div class="flex gap-4">
+            <x-select wire:model="account" :options="$this->accounts" />
+            {{-- {{ dd($this->months) }} --}}
+            <x-dropdown>
+                <x-slot:trigger>
+                    <x-button label="Período" icon="o-calendar" class="btn-neutral" />
+                </x-slot:trigger>
+
+                <div class="p-4 grid grid-cols-2 gap-2">
+                    <x-button wire:click="$set('range', 7)" label="Últimos 7 dias" class="btn-neutral"/>
+                    <x-button wire:click="$set('range', 15)" label="Últimos 15 dias" class="btn-neutral" />
+                    <x-button wire:click="$set('range', 30)" label="Últimos 30 dias" class="btn-neutral" />
+                    <x-button wire:click="$set('range', 60)" label="Últimos 60 dias" class="btn-neutral" />
+
+                    <div class="col-span-2" @click.stop="">
+                        <x-datepicker label="Período personalizado" wire:model="dateRange" icon="o-calendar" :config="$configDatePicker"/>
+                    </div>
+                    
+                    <div class="flex-1">
+                        <x-select label="Mês" wire:model="month" :options="$this->months" @click.stop=""
+                            class="flex-1" />
+                    </div>
+                    <div class="flex-1">
+                        <x-select label="Ano" wire:model="year" :options="$this->years" @click.stop="" class="flex-1" />
+                    </div>
+
+                    <x-button label="Filtrar" class="btn-primary col-span-2" wire:click="filter"/>
+                </div>
+            </x-dropdown>
+        </div>
+
+        <div class="p-4 mt-4 border-1 rounded-xl">
+            {{-- {{dd($rows)}} --}}
+            <x-table :rows="$this->transactions" :headers="$headers" striped />
+        </div>
+    </div>
 </div>
