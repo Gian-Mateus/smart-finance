7f937791f02e078a6c60b60dda87dbe505a87a31
Gian Mateus <gian.m.silver@gmail.com>
2025-11-16 21:46:18 -0300

    Add profile edit UI and bump dependencies

--- app/Livewire/Pages/Profile/Index.php
+++ app/Livewire/Pages/Profile/Index.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire\Pages\Profile;
 
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Index extends Component
 {
-    public function render()
+    public $name;
+    public $email;
+
+    public function updateName()
     {
-        return view('livewire.pages.profile.index');
+        // dd($this->name);
+        $user = Auth::user();
+        $user->name = $this->name;
+        $user->save();
+    }
+
+    public function mount()
+    {
+        $this->name = Auth::user()->name;
+        $this->email = Auth::user()->email;
+    }
+
+    /**
+     * @return View
+     */
+    public function render(): View
+    {
+        return \view('livewire.pages.profile.index');
     }
 }
--- resources/views/livewire/pages/profile/index.blade.php
+++ resources/views/livewire/pages/profile/index.blade.php
@@ -1,7 +1,80 @@
 <div>
     <div class="flex gap-4 mt-4">
-        <img src="" alt="" class="rounded-full max-w-sm max-h-sm h-24 w-24 ">
-        <h1 class="font-bold text-2xl">Teste</h1>
+        <img src="https://picsum.photos/200/300" alt="" class="rounded-full max-w-sm max-h-sm h-24 w-24 ">
+        <h1 class="font-bold text-2xl flex items-center">{{ $name }}</h1>
     </div>
     <x-menu-separator />
+    <div>
+        <div class="flex flex-col">
+            <div x-data="{ isEditing: false, originalName: @js($name) }">      
+                <x-input 
+                    label="Nome" 
+                    x-bind:disabled="!isEditing"
+                    wire:model="name"
+                    class="disabled:text-base-content cursor-default w-min"
+                >
+                    <x-slot:append>
+                        <div class="flex join-item">
+                            <x-button 
+                                icon="o-pencil" 
+                                x-show="!isEditing" 
+                                @click="isEditing = true"
+                                class="btn-secondary"
+                            />
+                            <x-button 
+                                icon="o-check" 
+                                x-show="isEditing"
+                                class="btn-success"
+                                wire:click="updateName"
+                                @click="isEditing = false"
+                            />
+                            <x-button 
+                                icon="o-x-mark" 
+                                x-show="isEditing" 
+                                @click="isEditing = false; $wire.set('name', originalName)"
+                                class="btn-error"
+                            />
+                        </div>
+                    </x-slot:append>
+                </x-input>
+            </div>
+            
+            <div x-data="{ isEditing: false, originalEmail: @js($email) }">    
+                <x-input 
+                    label="E-mail" 
+                    x-bind:disabled="!isEditing"
+                    wire:model="email"
+                    class="disabled:text-base-content cursor-default w-min"
+                >
+                    <x-slot:append>
+                        <div class="flex join-item">
+                            <x-button 
+                                icon="o-pencil" 
+                                x-show="!isEditing" 
+                                @click="isEditing = true"
+                                class="btn-secondary"
+                            />
+                            <x-button 
+                                icon="o-check" 
+                                x-show="isEditing"
+                                class="btn-success"
+                                wire:click="updateEmail"
+                                @click="isEditing = false"
+                            />
+                            <x-button 
+                                icon="o-x-mark" 
+                                x-show="isEditing" 
+                                @click="isEditing = false; $wire.set('email', originalEmail)"
+                                class="btn-error"
+                            />
+                        </div>
+                    </x-slot:append>
+                </x-input>
+            </div>
+        </div>
+        
+        <div class="mt-10">
+            <x-button label="Redefinir senha" class="btn-primary" icon="o-key"/>
+        </div>
+    </div>
 </div>
